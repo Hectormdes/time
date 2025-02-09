f16baf3b5539735d279932dc484ed97205e41788
Hectormdes <118676191+Hectormdes@users.noreply.github.com>
2025-02-09 13:06:31 -0400

    ok

---
+++ good_2.py
@@ -0,0 +1,11 @@
+f = " this is a book"
+print(len("f"))
+for i in f:
+	print(i)
+# the following
+def read_string():
+	print(f"repeating the string{f}")
+read_string()
+x = f.index("this")
+print(x)
+print(f[1:3])
